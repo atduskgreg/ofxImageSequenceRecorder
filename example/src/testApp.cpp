c2f434b3b7cf6a2daadb4427aa826c3f6fda1f1b
Greg Borenstein <greg@mfdz.com>
2012-07-09 14:20:22 -0400

    initial commit

---
+++ example/src/testApp.cpp
@@ -0,0 +1,93 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    cam.initGrabber(640, 480);
+    recording = false;
+     
+    currentFrame.allocate(640, 480, OF_IMAGE_COLOR);
+    
+    recorder.setPrefix(ofToDataPath("recording1/frame_")); // this directory must already exist
+    recorder.setFormat("jpg"); // png is really slow but high res, bmp is fast but big, jpg is just right
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    cam.grabFrame();
+	
+	if (cam.isFrameNew() && recording){
+        recorder.addFrame(cam);
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    ofBackground(125);
+    cam.draw(0,0);
+    
+    ofSetColor(255);
+    
+    stringstream c;
+    c << "Recording: " << recording << "\n" << "Queue Size: " << recorder.q.size() << endl;
+    
+    ofDrawBitmapString(c.str(), 650, 10);
+}
+
+void testApp::exit(){
+    recorder.waitForThread();
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+    if(key == 'r'){
+        recording = !recording;
+    }
+    
+    if(key == 't'){
+        if(recorder.isThreadRunning()){
+            recorder.stopThread();
+        } else {
+            recorder.startThread(false, true);   
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofxImageSequenceRecorder.h
@@ -0,0 +1,73 @@
+/*
+
+Based on code by Memo from this thread:
+http://forum.openframeworks.cc/index.php?topic=1687.0
+
+*/
+
+#include "ofMain.h"    
+
+typedef struct { 
+    string fileName;    
+    ofPixels image;
+} QueuedImage;
+
+class ofxImageSequenceRecorder : public ofThread {    
+public:    
+  
+    int counter;  
+    queue<QueuedImage> q;
+    string prefix;
+    string format;
+      
+    ofxImageSequenceRecorder(){  
+        counter=0;  
+        
+    }  
+    
+    void setPrefix(string pre){
+        prefix = pre;
+    }
+    
+    void setFormat(string fmt){
+        format = fmt;
+    }
+       
+    void threadedFunction() {    
+        while(isThreadRunning()) {
+            if(!q.empty()){
+                QueuedImage i = q.front();
+                ofSaveImage(i.image, i.fileName);
+                q.pop();
+            }
+        }
+        
+
+        
+    }   
+    
+    void addFrame(ofImage &img){
+        addFrame(img.getPixelsRef());
+    }
+    
+    void addFrame(ofVideoGrabber &cam){
+        addFrame(cam.getPixelsRef());
+    }
+    
+    void addFrame(ofPixels imageToSave) {  
+
+        
+        
+        char fileName[100]; 
+        sprintf(fileName,  "%s%.4i.%s" , prefix.c_str(), counter, format.c_str());     
+        counter++;   
+        
+        QueuedImage qImage;
+        
+        qImage.fileName = fileName;    
+        qImage.image = imageToSave; 
+        
+        q.push(qImage);
+        
+    }    
+};  
